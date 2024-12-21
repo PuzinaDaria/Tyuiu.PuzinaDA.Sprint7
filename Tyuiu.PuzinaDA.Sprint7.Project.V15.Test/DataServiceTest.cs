b9295462fbdd8122955cc095d4621c37f7beddd1
Tyuiu.puzinada@outlook.com <dariapuzina7b@gmail.com>
2024-12-21 18:53:45 +0500

    Sprint 7

--- Tyuiu.PuzinaDA.Sprint7.Project.V15.Test/DataServiceTest.cs
+++ Tyuiu.PuzinaDA.Sprint7.Project.V15.Test/DataServiceTest.cs
@@ -6,6 +6,20 @@ namespace Tyuiu.PuzinaDA.Sprint7.Project.V15.Test
     public class DataServiceTest
     {
         [TestMethod]
+        public void MatrixChecked()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\daria\source\repos\Tyuiu.PuzinaDA.Sprint7\Материал\База данных\Тест.csv";
+            string[,] res = ds.GetMatrix(path);
+            string[,] wait =
+            {
+                {"24245",  "Никитич Никита Никитович", "895458772", "20.02.2024", "бульвар Славы, 92", "45000", "PR-менеджер"},
+                {"17853", "Николаев Николай Николаевич", "245646638", "28.01.2024", "пер. Косиора, 14", "25000", "Уборщица"},
+                {"98164", "Дмитриев Дмитрий Дмитриевич", "450596744", "19.11.2023", "пр. Славы, 37", "35000", "Электрик"}
+            };
+            CollectionAssert.AreEqual(res, wait);
+
+        }
         public void TrudDogovorChecked()
         {
             DataService ds = new DataService();
